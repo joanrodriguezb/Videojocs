ff5bc84e85b8b567649272fe9491a1f52f59e058
claaudiiaa <claudiafarina@inspladelestany.cat>
2022-12-02 17:14:17 +0100

    proves automàtiques

--- test/NotaTest.java
+++ test/NotaTest.java
@@ -2,11 +2,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class NotaTest {
 
+    public static final int NOTA = 8;
+
     @org.junit.jupiter.api.Test
     void setIgetNota() {
         Nota notaTest = new Nota();
-        notaTest.setNota(8);
-        assert (notaTest.getNota() == 5);
+        notaTest.setNota(NOTA);
+        assert (notaTest.getNota() == NOTA);
     }
 
 
